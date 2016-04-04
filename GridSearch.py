39f9c7f38be791df04962f168198bb21a3a48445
Robert Lutz <robertlutzi@gmail.com>
2016-04-03 22:47:56 -0400

    refactor: Change print conditions during search
     - now it only prints the hyperparameters if they are the best to date

--- GridSearch.py
+++ GridSearch.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pandas as pd
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.grid_search import RandomizedSearchCV
+from sklearn.metrics import roc_auc_score
 
-def GridSearch(classifier, paramdict, iters, X, y):
+
+def GridSearch(classifier, paramdict, iters, X, y, X_reserve, y_reserve):
     '''
     Takes as input:
         classifier: the sklearn classifier to investigate
@@ -25,7 +28,33 @@ def GridSearch(classifier, paramdict, iters, X, y):
     '''
     np.random.seed(42)
     kfcv = StratifiedKFold(y, n_folds=5, shuffle=True)
-    gs = RandomizedSearchCV(classifier, paramdict, n_iter=iters, cv=kfcv,
-                            scoring='roc_auc')
-    gs.fit(X, y)
-    return gs.best_estimator_, gs.best_params_, gs.best_score_
+
+    best_score = 0
+
+    for _ in xrange(iters):
+        if 'n_estimators' in paramdict:
+            classifier.n_estimators = paramdict['n_estimators'].rvs()
+        if 'max_depth' in paramdict:
+            classifier.max_depth = paramdict['max_depth'].rvs()
+        if 'learning_rate' in paramdict:
+            classifier.learning_rate = paramdict['learning_rate'].rvs()
+        if 'min_samples_leaf' in paramdict:
+            classifier.min_samples_leaf = paramdict['min_samples_leaf'].rvs()
+        if 'subsample' in paramdict:
+            classifier.subsample = paramdict['subsample'].rvs()
+        if 'C' in paramdict:
+            classifier.C = paramdict['C'].rvs()
+        if 'gamma' in paramdict:
+            classifier.gamma = paramdict['gamma'].rvs()
+        scores = []
+        for fit, val in kfcv:
+            classifier.fit(X.iloc[fit], y.iloc[fit])
+            scores.append(roc_auc_score(pd.concat([y.iloc[val], y_reserve]), classifier.predict_proba(pd.concat([X.iloc[val], X_reserve]))[:,1]))
+        score = 1.*sum(scores)/len(scores)
+        if score > best_score:
+            print score
+            print classifier.get_params()
+            best_score = score
+            best_params = classifier.get_params()
+    best_estimator = classifier.set_params(**best_params)
+    return best_estimator, best_params, best_score
