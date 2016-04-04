d7ef318acc21115e96847fd279ba5c736ee8e1db
Robert Lutz <robertlutzi@gmail.com>
2016-04-04 15:33:50 -0400

    fix: Increase number of grid search iterations
    - also some refactoring of code

--- GradientBooster.py
+++ GradientBooster.py
@@ -5,19 +5,30 @@ from dataloader import import_data
 from GridSearch import GridSearch
 import numpy as np
 import pandas as pd
+import scipy
 from sklearn.ensemble import GradientBoostingClassifier
+
 
 #===================================prep data==================================
 
-X_train, y_train, X_test, id_test = import_data('train.csv', 'test.csv', 'TARGET', 'ID', verbose=True)
+np.random.seed(42)
+
+target_col = 'TARGET'
+id_col = 'ID'
+
+X_train = pd.read_csv('./EngineeredData/Xtrain.csv')
+y_train = pd.read_csv('./EngineeredData/ytrain.csv')[target_col]
+X_test = pd.read_csv('./EngineeredData/Xtest.csv')
+id_test = pd.read_csv('./EngineeredData/idtest.csv')[id_col]
 
 #==========================Gradient Boost Classifier===========================
 
-params = {'n_estimators': scipy.stats.geom(1/150.),
-            'max_depth': scipy.stats.randint(3,10),
-            'learning_rate': scipy.stats.expon(0.01),
-            'min_samples_leaf': scipy.stats.geom(1/5.),
-            'subsample': scipy.stats.beta(2,1)
+params = {
+            'n_estimators'      :       scipy.stats.geom(1/138.),
+            'max_depth'         :       scipy.stats.randint(2,7),
+            'learning_rate'     :       scipy.stats.expon(0, 0.038),
+            'min_samples_leaf'  :       scipy.stats.geom(1/9.),
+            'subsample'         :       scipy.stats.beta(17,3)
             }
 
 clf = GradientBoostingClassifier()
@@ -25,14 +36,12 @@ clf = GradientBoostingClassifier()
 best_gb, best_hyperparams, best_gb_auc = GridSearch(
                         classifier      =       clf,
                         paramdict       =       params,
-                        iters           =       25,
+                        iters           =       243,
                         X               =       X_train,
-                        y               =       y_train
+                        y               =       y_train,
+                        X_reserve       =       None,
+                        y_reserve       =       None
             )
 
 print 'Best GB hyperparams: %s' % best_hyperparams
 print 'GB AUC: %s' % best_gb_auc
-
-best_gb.fit(X_train, y_train)
-# predicting
-gb_pred = best_gb.predict_proba(X_test)[:,1]
