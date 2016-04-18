ac63b09d73e97e2cbed10642f1c47f71f57f0e2a
Robert Lutz <robertlutzi@gmail.com>
2016-04-17 22:31:48 -0400

    refactor: Simplify the setting of hyperparameters
     - combine former set_params and randomize_params into one substantially better
     function
     - rolled out only to Stacker.py for now. Just in case it bugs out

--- Stacker.py
+++ Stacker.py
@@ -25,69 +25,15 @@ def set_params(classifier, paramdict):
     Returns:
         - an estimator with hyperparemeters fixed
     '''
-    if 'n_estimators' in paramdict:
-        classifier.n_estimators = paramdict['n_estimators']
-    if 'max_depth' in paramdict:
-        classifier.max_depth = paramdict['max_depth']
-    if 'learning_rate' in paramdict:
-        classifier.learning_rate = paramdict['learning_rate']
-    if 'min_samples_leaf' in paramdict:
-        classifier.min_samples_leaf = paramdict['min_samples_leaf']
-    if 'subsample' in paramdict:
-        classifier.subsample = paramdict['subsample']
-    if 'C' in paramdict:
-        classifier.C = paramdict['C']
-    if 'gamma' in paramdict:
-        classifier.gamma = paramdict['gamma']
-    if 'colsample_bytree' in paramdict:
-        classifier.colsample_bytree = paramdict['colsample_bytree']
-    if 'intercept_scaling' in paramdict:
-        classifier.intercept_scaling = paramdict['intercept_scaling']
-    if 'base_score' in paramdict:
-        classifier.base_score = paramdict['base_score']
-    if 'scale_pos_weight' in paramdict:
-        classifier.scale_pos_weight = paramdict['scale_pos_weight']
-    if 'min_child_weight' in paramdict:
-        classifier.min_child_weight = paramdict['min_child_weight']
-    return classifier
-
-#==============================================================================
-
-def randomize_params(classifier, paramdict):
-    '''
-    Takes as input:
-        classifier: an estimator object (scikit-learn compatible)
-        paramdict: a dictionary keyed by hyperparameter names with random
-        distribution objects as values
-    What it does:
-        Associates each hyperparameter in the paramdict andomly selects hyperparameters for the chosen estimator.
-    Returns:
-        - an estimator with hyperparemeters fixed
-    '''
-    if 'n_estimators' in paramdict:
-        classifier.n_estimators = int(paramdict['n_estimators'].rvs())
-    if 'max_depth' in paramdict:
-        classifier.max_depth = int(paramdict['max_depth'].rvs())
-    if 'learning_rate' in paramdict:
-        classifier.learning_rate = paramdict['learning_rate'].rvs()
-    if 'min_samples_leaf' in paramdict:
-        classifier.min_samples_leaf = int(paramdict['min_samples_leaf'].rvs())
-    if 'subsample' in paramdict:
-        classifier.subsample = paramdict['subsample'].rvs()
-    if 'C' in paramdict:
-        classifier.C = paramdict['C'].rvs()
-    if 'gamma' in paramdict:
-        classifier.gamma = paramdict['gamma'].rvs()
-    if 'colsample_bytree' in paramdict:
-        classifier.colsample_bytree = paramdict['colsample_bytree'].rvs()
-    if 'intercept_scaling' in paramdict:
-        classifier.intercept_scaling = paramdict['intercept_scaling'].rvs()
-    if 'base_score' in paramdict:
-        classifier.base_score = paramdict['base_score'].rvs()
-    if 'scale_pos_weight' in paramdict:
-        classifier.scale_pos_weight = paramdict['scale_pos_weight'].rvs()
-    if 'min_child_weight' in paramdict:
-        classifier.min_child_weight = paramdict['min_child_weight'].rvs()
+    for param in paramdict:
+        if type(paramdict[param]) in (int, float):
+            setattr(classifier, param, paramdict[param])
+        else:
+            v = paramdict[param].rvs()
+            if param in ('max_depth', 'min_samples_leaf', 'n_estimators'):
+                setattr(classifier, param, int(v))
+            else:
+                setattr(classifier, param, v)
     return classifier
 
 #==============================================================================
@@ -162,7 +108,7 @@ def generalized_CV(method, classifier, paramdict, iters, folds,
     estimates = pd.DataFrame()
     predictions = pd.DataFrame()
     for _ in xrange(iters):
-        esty = randomize_params(classifier, paramdict)
+        esty = set_params(classifier, paramdict)
         training_probs = pd.Series()
         for fit, val in folds:
             # fit this model using this fitting subset
@@ -201,7 +147,7 @@ def generalized_CV(method, classifier, paramdict, iters, folds,
         return estimates, predictions, weights, y_train_shuffled, params
 
 #===================================prep data==================================
-'''
+
 target_col = 'TARGET'
 id_col = 'ID'
 X_train = pd.read_csv('./EngineeredData/Xtrain.csv')
@@ -252,15 +198,15 @@ estimates.to_csv('./Level1Data/Xtrain.csv', index=False)
 pd.DataFrame({target_col:y_train}).to_csv('./Level1Data/ytrain.csv', index=False)
 predictions.to_csv('./Level1Data/Xtest.csv', index=False)
 pd.DataFrame({id_col:id_test}).to_csv('./Level1Data/idtest.csv', index=False)
-'''
+
 #================Level 1 Estimator: Logistic Regression========================
 
-target_col = 'TARGET'
-id_col = 'ID'
-estimates = pd.read_csv('./Level1Data/Xtrain.csv')
-y_train = pd.read_csv('./Level1Data/ytrain.csv')[target_col]
-predictions = pd.read_csv('./Level1Data/Xtest.csv')
-id_test = pd.read_csv('./Level1Data/idtest.csv')[id_col]
+#target_col = 'TARGET'
+#id_col = 'ID'
+#estimates = pd.read_csv('./Level1Data/Xtrain.csv')
+#y_train = pd.read_csv('./Level1Data/ytrain.csv')[target_col]
+#predictions = pd.read_csv('./Level1Data/Xtest.csv')
+#id_test = pd.read_csv('./Level1Data/idtest.csv')[id_col]
 np.random.seed(3)
 kfcv1 = StratifiedKFold(y_train, n_folds=5, shuffle=True)
 estim_cols = estimates.columns
@@ -295,63 +241,6 @@ for i, result in enumerate(ordered_cols):
 best_estimator = set_params(l1Clf, best_params)
 print 'We decided upon %s XGB results' % len(master_cols)
 
-#================Level 1 Estimator: XGB========================
-'''
-target_col = 'TARGET'
-id_col = 'ID'
-estimates = pd.read_csv('./Level1Data/Xtrain.csv')
-y_train = pd.read_csv('./Level1Data/ytrain.csv')[target_col]
-predictions = pd.read_csv('./Level1Data/Xtest.csv')
-id_test = pd.read_csv('./Level1Data/idtest.csv')[id_col]
-np.random.seed(3)
-kfcv1 = StratifiedKFold(y_train, n_folds=5, shuffle=True)
-score = 0.840513226531
-nest = 154
-learning = 0.044485809252364054
-depth = 2
-sub = 0.6276496064148965
-csbt = 0.42368346812722996
-g = 0
-a = 0
-spw = 24.377958783463924
-mcw = 1.334711811795449
-base = 0.6879643409508887
-
-l1Clf = XGBClassifier()
-estim_cols = estimates.columns
-master_cols = []
-top_score = 0
-for i, result in enumerate(estim_cols):
-    params = {
-            'n_estimators'      :       scipy.stats.norm(nest, nest/3),
-            'learning_rate'     :       scipy.stats.norm(learning, learning/3),
-            'max_depth'         :       scipy.stats.norm(depth,depth/3.),
-            'subsample'         :       scipy.stats.norm(sub, sub/3),
-            #'colsample_bytree'  :       scipy.stats.norm(csbt, csbt/3),
-            'gamma'             :       scipy.stats.norm(g, g/3),
-            'reg_alpha'         :       scipy.stats.norm(a, a/3),
-            'scale_pos_weight'  :       scipy.stats.norm(spw, spw/3),
-            'min_child_weight'  :       scipy.stats.norm(mcw, mcw/3),
-            'base_score'        :       scipy.stats.beta(base/(1-base), 1)
-        }
-    estimator, params, score = generalized_CV(
-                    method        =       'GridSearch',
-                    classifier    =       l1Clf,
-                    paramdict     =       params,
-                    iters         =       25,
-                    folds         =       kfcv1,
-                    X_train       =       estimates[master_cols + [result]],
-                    y_train       =       y_train
-            )
-    if score > top_score:
-        top_score = score
-        best_params = params
-        master_cols.append(result)
-    print 'WIP: from %s estimates we choose %s' % (i+1, len(master_cols))
-best_estimator = set_params(l1Clf, best_params)
-print 'We decided upon %s XGB results' % len(master_cols)
-
-'''
 #==============================================================================
 
 print 'The Level1 training data ROC-AuC score is %s' % top_score
