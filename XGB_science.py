46adce307b2589696d145c5e80ff6d527a66284e
Robert Lutz <robertlutzi@gmail.com>
2016-04-17 22:29:24 -0400

    feat: Introduce continuous improvement cycling
     - in XGB_science.py for finding optimal L0 XGB classifier

--- XGB_science.py
+++ XGB_science.py
@@ -216,177 +216,162 @@ y_train = pd.read_csv('./EngineeredData/ytrain.csv')[target_col]
 X_test = pd.read_csv('./EngineeredData/Xtest.csv')
 id_test = pd.read_csv('./EngineeredData/idtest.csv')[id_col]
 
-np.random.seed(42)
+np.random.seed(3)
 kfcv0 = StratifiedKFold(y_train, n_folds=4, shuffle=True)
 #X_fit, X_val, y_fit, y_val = train_test_split(X_train, y_train,
 #                                    test_size=0.25, stratify=y_train)
 
 
-X_train = pd.read_csv('./Level1Data/Xtrain.csv')
-y_train = pd.read_csv('./Level1Data/ytrain.csv')[target_col]
+#X_train = pd.read_csv('./Level1Data/Xtrain.csv')
+#y_train = pd.read_csv('./Level1Data/ytrain.csv')[target_col]
 
 #=======================Initial Guess at Hyperparameters=======================
 
-Clf = XGBClassifier(        n_estimators        =       50,
-                            gamma               =       0,
-                            scale_pos_weight    =       10,
-                            subsample           =       0.8,
-                            colsample_bytree    =       0.8,
-                            learning_rate       =       0.1)
+params =            {       'max_depth'           :       5,
+                            'min_child_weight'    :       1,
+                            'gamma'               :       1,
+                            'n_estimators'        :       50,
+                            'learning_rate'       :       0.1,
+                            'subsample'           :       0.8,
+                            'reg_alpha'           :       1,
+                            'colsample_bytree'    :       0.8,
+                            'base_score'          :       0.5,
+                            'scale_pos_weight'    :       10
+                    }
+Clf = set_params(XGBClassifier(), params)
 
-#================Tuning Round 1: max_depth & min_child_weight==================
-print 'Grid Searching max_depth and min_child_weight'
-params = {
-            'max_depth'         :       scipy.stats.norm(6,2),
-            'min_child_weight'  :       scipy.stats.expon(0,1)
-            }
-estimator, parameters, score = generalized_CV(
-                        method                  =       'GridSearch',
-                        classifier              =       Clf,
-                        paramdict               =       params,
-                        iters                   =       25,
-                        folds                   =       kfcv0,
-                        X_train                 =       X_train,
-                        y_train                 =       y_train,
-                        X_test                  =       X_test
-            )
-Clf = estimator
-print 'Done with max_depth and min_child_weight'
-print Clf.get_params()
-#===========================Tuning Round 2: gamma==============================
-print 'Grid Searching gamma'
-params = {
-            'gamma'             :       scipy.stats.expon(0, 1)
-            }
-estimator, parameters, score = generalized_CV(
-                        method                  =       'GridSearch',
-                        classifier              =       Clf,
-                        paramdict               =       params,
-                        iters                   =       5,
-                        folds                   =       kfcv0,
-                        X_train                 =       X_train,
-                        y_train                 =       y_train,
-                        X_test                  =       X_test,
-                        best_score              =       score
-            )
-Clf = estimator
-print 'Done with gamma'
-print Clf.get_params()
-#================Tuning Round 3: subsample and colsample_bytree================
-print 'Grid Searching subsample and colsample_bytree'
-params = {
-            'subsample'         :       scipy.stats.beta(4,1),
-            'colsample_bytree'  :       scipy.stats.beta(4,1),
-            }
-estimator, parameters, score = generalized_CV(
-                        method                  =       'GridSearch',
-                        classifier              =       Clf,
-                        paramdict               =       params,
-                        iters                   =       25,
-                        folds                   =       kfcv0,
-                        X_train                 =       X_train,
-                        y_train                 =       y_train,
-                        X_test                  =       X_test,
-                        best_score              =       score
-            )
-Clf = estimator
-print 'Done with subsample and colsample_bytree'
-print Clf.get_params()
-#============================Tuning Round 4: alpha=============================
-print 'Grid Searching alpha'
-params = {
-            'reg_alpha'         :       scipy.stats.expon(0, 0.01)
-            }
-estimator, parameters, score = generalized_CV(
-                        method                  =       'GridSearch',
-                        classifier              =       Clf,
-                        paramdict               =       params,
-                        iters                   =       5,
-                        folds                   =       kfcv0,
-                        X_train                 =       X_train,
-                        y_train                 =       y_train,
-                        X_test                  =       X_test,
-                        best_score              =       score
-            )
-Clf = estimator
-print 'Done with alpha'
-print Clf.get_params()
-#==============Tuning Round 5: n_estimators and learning_rate==================
-print 'Grid Searching n_estimators and learning_rate'
-params = {
-            'n_estimators'      :       scipy.stats.expon(20, 30),
-            'learning_rate'     :       scipy.stats.expon(0, 0.03),
-            }
-estimator, parameters, score = generalized_CV(
-                        method                  =       'GridSearch',
-                        classifier              =       Clf,
-                        paramdict               =       params,
-                        iters                   =       25,
-                        folds                   =       kfcv0,
-                        X_train                 =       X_train,
-                        y_train                 =       y_train,
-                        X_test                  =       X_test,
-                        best_score              =       score
-            )
-Clf = estimator
-print 'Done with n_estimators and learning_rate'
-print Clf.get_params()
-#==============Tuning Round 5: base score and scale_pos_weight=================
-print 'Grid Searching base_score and scale_pos_weight'
-params = {
-            'base_score'        :       scipy.stats.beta(1,1),
-            'scale_pos_weight'  :       scipy.stats.expon(0, 10),
-            }
-estimator, parameters, score = generalized_CV(
-                        method                  =       'GridSearch',
-                        classifier              =       Clf,
-                        paramdict               =       params,
-                        iters                   =       25,
-                        folds                   =       kfcv0,
-                        X_train                 =       X_train,
-                        y_train                 =       y_train,
-                        X_test                  =       X_test,
-                        best_score              =       score
-            )
-Clf = estimator
-print 'Done with base_score and scale_pos_weight'
-print Clf.get_params()
-#==============================Eternal Tuning =================================
+score = 0
 
-score = 0.840590223128
-parameters = {'reg_alpha': 0, 'colsample_bytree': 0.6584220199507204, 'silent': True, 'colsample_bylevel': 1, 'scale_pos_weight': 1.3490671869064994, 'learning_rate': 0.03209999716433319, 'missing': None, 'max_delta_step': 0, 'nthread': -1, 'base_score': 0.0854251661681438, 'n_estimators': 164, 'subsample': 0.8622924469301847, 'reg_lambda': 1, 'seed': 0, 'min_child_weight': 0.08016798146134174, 'objective': 'binary:logistic', 'max_depth': 6, 'gamma': 0.5182180124120709}
+while True:
+    #=============Tuning Round 1: max_depth & min_child_weight=================
 
-nest = parameters['n_estimators']
-learning = parameters['learning_rate']
-depth = parameters['max_depth']
-sub = parameters['subsample']
-csbt = parameters['colsample_bytree']
-g = parameters['gamma']
-a = parameters['reg_alpha']
-spw = parameters['scale_pos_weight']
-mcw = parameters['min_child_weight']
-base = parameters['base_score']
+    print 'Grid Searching max_depth and min_child_weight'
+    depth = params['max_depth']
+    mcw = params['min_child_weight']
+    params = {
+                'max_depth'         :       scipy.stats.norm(depth, depth/3),
+                'min_child_weight'  :       scipy.stats.expon(0, mcw)
+                }
+    estimator, params, score = generalized_CV(
+                            method                  =       'GridSearch',
+                            classifier              =       Clf,
+                            paramdict               =       params,
+                            iters                   =       25,
+                            folds                   =       kfcv0,
+                            X_train                 =       X_train,
+                            y_train                 =       y_train,
+                            X_test                  =       X_test,
+                            best_score              =       score
+                )
+    Clf = estimator
+    print 'Done with max_depth and min_child_weight'
 
-params = {
-            'n_estimators'      :       scipy.stats.norm(nest, nest/3),
-            'learning_rate'     :       scipy.stats.norm(learning, learning/3),
-            'max_depth'         :       scipy.stats.norm(depth,depth/3.),
-            'subsample'         :       scipy.stats.norm(sub, sub/3),
-            'colsample_bytree'  :       scipy.stats.norm(csbt, csbt/3),
-            'gamma'             :       scipy.stats.norm(g, g/3),
-            'reg_alpha'         :       scipy.stats.norm(a, a/3),
-            'scale_pos_weight'  :       scipy.stats.norm(spw, spw/3),
-            'min_child_weight'  :       scipy.stats.norm(mcw, mcw/3),
-            'base_score'        :       scipy.stats.norm(base, base/3)
-        }
-estimator, parameters, score = generalized_CV(
-                        method                  =       'GridSearch',
-                        classifier              =       Clf,
-                        paramdict               =       params,
-                        iters                   =       100000000000,
-                        folds                   =       kfcv0,
-                        X_train                 =       X_train,
-                        y_train                 =       y_train,
-                        X_test                  =       X_test,
-                        best_score              =       score
-            )
+    #=========================Tuning Round 2: gamma============================
+
+    print 'Grid Searching gamma'
+    g = params['gamma']
+    params = {
+                'gamma'             :       scipy.stats.expon(0, g)
+                }
+    estimator, params, score = generalized_CV(
+                            method                  =       'GridSearch',
+                            classifier              =       Clf,
+                            paramdict               =       params,
+                            iters                   =       5,
+                            folds                   =       kfcv0,
+                            X_train                 =       X_train,
+                            y_train                 =       y_train,
+                            X_test                  =       X_test,
+                            best_score              =       score
+                )
+    Clf = estimator
+    print 'Done with gamma'
+
+    #==============Tuning Round 3: subsample and colsample_bytree==============
+
+    print 'Grid Searching subsample and colsample_bytree'
+    sub = params['subsample']
+    csbt = params['colsample_bytree']
+    params = {
+                'subsample'         :       scipy.stats.beta(sub/(1-sub),1),
+                'colsample_bytree'  :       scipy.stats.beta(csbt/(1-csbt),1)
+                }
+    estimator, params, score = generalized_CV(
+                            method                  =       'GridSearch',
+                            classifier              =       Clf,
+                            paramdict               =       params,
+                            iters                   =       25,
+                            folds                   =       kfcv0,
+                            X_train                 =       X_train,
+                            y_train                 =       y_train,
+                            X_test                  =       X_test,
+                            best_score              =       score
+                )
+    Clf = estimator
+    print 'Done with subsample and colsample_bytree'
+
+    #==========================Tuning Round 4: alpha===========================
+
+    print 'Grid Searching alpha'
+    a = params['reg_alpha']
+    params = {
+                'reg_alpha'         :       scipy.stats.expon(0, a)
+                }
+    estimator, params, score = generalized_CV(
+                            method                  =       'GridSearch',
+                            classifier              =       Clf,
+                            paramdict               =       params,
+                            iters                   =       5,
+                            folds                   =       kfcv0,
+                            X_train                 =       X_train,
+                            y_train                 =       y_train,
+                            X_test                  =       X_test,
+                            best_score              =       score
+                )
+    Clf = estimator
+    print 'Done with alpha'
+
+    #============Tuning Round 5: n_estimators and learning_rate================
+
+    print 'Grid Searching n_estimators and learning_rate'
+    nest = params['n_estimators']
+    lr = params['learning_rate']
+    params = {
+                'n_estimators'      :       scipy.stats.expon(0, nest),
+                'learning_rate'     :       scipy.stats.expon(0, lr),
+                }
+    estimator, params, score = generalized_CV(
+                            method                  =       'GridSearch',
+                            classifier              =       Clf,
+                            paramdict               =       params,
+                            iters                   =       25,
+                            folds                   =       kfcv0,
+                            X_train                 =       X_train,
+                            y_train                 =       y_train,
+                            X_test                  =       X_test,
+                            best_score              =       score
+                )
+    Clf = estimator
+    print 'Done with n_estimators and learning_rate'
+
+    #============Tuning Round 6: base score and scale_pos_weight===============
+
+    print 'Grid Searching base_score and scale_pos_weight'
+    base = params['base_score']
+    params = {
+                'base_score'        :       scipy.stats.beta(1,1),
+                'scale_pos_weight'  :       scipy.stats.expon(0, 10),
+                }
+    estimator, params, score = generalized_CV(
+                            method                  =       'GridSearch',
+                            classifier              =       Clf,
+                            paramdict               =       params,
+                            iters                   =       25,
+                            folds                   =       kfcv0,
+                            X_train                 =       X_train,
+                            y_train                 =       y_train,
+                            X_test                  =       X_test,
+                            best_score              =       score
+                )
+    Clf = estimator
+    print 'Done with base_score and scale_pos_weight'
